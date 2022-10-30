75db4b541af35f2b766c60a8e2c275ebc15c606f
imac-norch <fucskonorbi@gmail.com>
2022-10-30 16:38:44 +0100

    Remove reload and port from config

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ def search(keyword: str, timestamp: str, item_ids_to_skip: str=""):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
